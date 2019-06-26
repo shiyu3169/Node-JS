deee32d337729f9480f2589128e28c25da518da7
Shiyu Wang <shiyu3169@gmail.com>
2019-06-26 16:00:37 -0400

    rename file"

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -28,7 +28,17 @@ const path = require('path');
 // );
 
 // Read file
-fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// Rename file
+fs.rename(
+  path.join(__dirname, '/test', 'hello.txt'),
+  path.join(__dirname, '/test', 'helloworld.txt'),
+  err => {
+    if (err) throw err;
+    console.log('File renamed...');
+  }
+);
