914e8ed8113bc2e1a461fa90bfe40185b344d189
Shiyu Wang <shiyu3169@gmail.com>
2019-06-26 15:01:12 -0400

    path module

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -1,1 +1,10 @@
 const path = require('path');
+
+// Base file name
+console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
