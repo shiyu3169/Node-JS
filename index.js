6fac2b6f403aa2de6afec6e5d99b804b23d4b0fe
Shiyu Wang <shiyu3169@gmail.com>
2019-06-27 15:29:16 -0400

    ,

--- index.js
+++ index.js
@@ -31,6 +31,60 @@ const server = http.createServer((req, res) => {
   //   res.writeHead(200, { 'Content-Type': 'application/json' });
   //   res.end(JSON.stringify(users));
   // }
+
+  // Build file path
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content type
+  let contentType = 'text/html';
+
+  // Check ext and set content type
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  // Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+          }
+        );
+      } else {
+        // Server Error
+        res.writeHead(500);
+        res.end(`Server Error : ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
